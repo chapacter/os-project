eafe44ddca406b9f16654d38dc4de77ad7d3c04b
Chapacter <artem6453a@gmail.com>
2026-06-20 18:38:59 +0400

    маркеры для объектов (пока пустые)

---
+++ entity/components/tags.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PlayerMarker:
+    pass
+
+
+@dataclass
+class EnemyMarker:
+    pass
+
+
+@dataclass
+class BulletMarker:
+    pass
+
+
+@dataclass
+class LootMarker:
+    pass
+
+
+@dataclass
+class WeaponMarker:
+    pass
